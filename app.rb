40d177c0d72307be99912504e0077b961902c555
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-12 15:25:37 +0100

    Add cat path with image with border

--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
 require "sinatra"
+require 'shotgun'
+
+set :session_secret, "here be dragons"
 
 get '/' do
   "Kiril and Charlie's page"
@@ -6,4 +9,22 @@ end
 
 get '/secret' do
   "12345"
+end
+
+get '/super_secret' do
+  "Shhhh!"
+end
+
+get "/notsecret" do
+  "Woah!"
+end
+
+get "/thing" do
+  "it's a thing"
+end
+
+get "/cat" do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
