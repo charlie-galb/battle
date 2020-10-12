019334b6b7389ef1e0701d41e65c8db852938dad
Kiril <kiril@olgas-air.mynet>
2020-10-12 15:58:32 +0100

    added random name generator

--- app.rb
+++ app.rb
@@ -20,11 +20,9 @@ get "/notsecret" do
 end
 
 get "/thing" do
-  "it's a thing"
+  "it's a thingy"
 end
 
 get "/cat" do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
