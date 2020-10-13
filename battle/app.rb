359c880be95ceebaed81996afd07bd67c2751c13
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-13 16:21:55 +0100

    Add first feature test checking player name functionality (passes)

--- battle/app.rb
+++ battle/app.rb
@@ -2,6 +2,11 @@ require 'sinatra'
 
 class Battle < Sinatra::Base
   get '/' do
-    "Testing infrastructure working!"
+    erb :index
+  end
+  post '/names' do
+    @Player1_Name = params[:Player1_Name]
+    @Player2_Name = params[:Player2_Name]
+    erb :play
   end
 end
--- battle/spec/features/testing_infrastructure_spec.rb
+++ battle/spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,9 @@
 feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+  scenario 'Can enter name and seeing them printed out on screen' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in('Player1_Name', with: 'Me')
+    fill_in('Player2_Name', with: 'You')
+    click_on('Submit')
+    expect(page).to have_text("Welcome, Me and You")
   end
 end
